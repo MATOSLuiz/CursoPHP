9725556447dd61cc8df7b8c7b7c0124da7e80629
Luiz <luis231103@gmail.com>
2022-03-03 12:21:50 -0300

    Conceitos básicos

---
+++ 2.conceitosBasicos/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+//não é ase sensitive
+
+ECHO "teste <br>";
+EchO "teste2 <br>";
+ECho "Teste3 <br>";
+echo "Teste4 <br>";
+
+//é Case Sensitive
+
+$nome = "Luiz";
+$NOME = "Matheus";
+
+echo $nome;
+echo "<br>";
+echo $NOME;
+
+?>
