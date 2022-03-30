a0585e24b9150401a892fb1d3f898a5a58b30426
Luiz <luis231103@gmail.com>
2022-03-30 15:23:44 -0300

    Exercicio 12

---
+++ ExerciciosCurso/Exercicio12.php
@@ -0,0 +1,19 @@
+<?php
+
+$x = 15;
+$y = 10;
+
+$soma = $x + $y;
+echo $soma;
+echo "<br>";
+$subtracao = $x - $y;
+echo $subtracao;
+echo "<br>";
+$divisao = $x / $y;
+echo $divisao;
+echo "<br>";
+$multiplicacao = $x * $y;
+echo $multiplicacao;
+echo "<br>";
+
+?>
