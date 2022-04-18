7778c33f3781b728e6c2ea27552327b928ab84fa
Luiz <luis231103@gmail.com>
2022-04-18 13:34:53 -0300

    Exercicio34

---
+++ ExerciciosCurso/Exercicio34.php
@@ -0,0 +1,13 @@
+<?php 
+
+function parOuImpar($numero) {
+    if($numero % 2 == 0) {
+        echo "O número $numero  é par!";
+    } else {
+        echo "O número $numero é ímpar!";
+    }
+}
+
+parOuImpar(14);
+
+?>
