d235764147b73f57dd7c4c63a9957dfdec506da1
Luiz <luis231103@gmail.com>
2022-04-18 11:36:55 -0300

    Invocando funções

--- 9.funções/index.php
+++ 9.funções/index.php
@@ -8,4 +8,28 @@ function teste() {
 
 }
 
+//o php tem funções nativas como strlen, strtoupper...
+
+//sem parametros
+
+function subtracao() {
+
+    $a = 4;
+    $b = 5;
+
+    echo $b - $a . "<br>";
+}
+
+//chamando função
+
+subtracao();
+subtracao();
+
+//função nativa strtoupper
+
+echo strtoupper("teste");
+
+//Dica: iniciar todas as funções do programa no topo do arquivo.
+
+
 ?>
