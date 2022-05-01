f5d90a343c9cf3a66a686ced1680c0a49d92b038
Luiz <luis231103@gmail.com>
2022-05-01 15:28:19 -0300

    Retornando multiplos dados

--- 9.funções/index.php
+++ 9.funções/index.php
@@ -118,7 +118,20 @@ function sum($a, $b) {
 
 sum(15, 15);
 
+//retornando vários dados
 
+function alteraDados($nome, $idade) {
+    $nome = "Sr. $nome";
+    $idade = "$idade anos";
 
+    return [$nome, $idade];
+}
 
+$dados = alteraDados("Luiz", 18);
+
+print_r($dados);
+
+echo "<br>";
+
+echo "Olá $dados[0], Você tem $dados[1]";
 ?>
