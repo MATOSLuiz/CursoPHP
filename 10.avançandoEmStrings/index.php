c69150a13a92cfaa2047ae06f0591d3a196d0807
Luiz <luis231103@gmail.com>
2022-05-02 23:13:45 -0300

    Interpolação de Variaveis

---
+++ 10.avançandoEmStrings/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+//Interpolação de Variaveis
+
+$nomeCachorro = "BOB";
+$idadeCachorro = 10;
+
+//Sempre lembrar de seguir o padrão do projeto se começar de um jeito termine desse jeito e nunca misture.
+
+echo "Meu cachorro se chama $nomeCachorro e tem $idadeCachorro aninhos";
+echo "Meu cachorro se chama {$nomeCachorro} e tem {$idadeCachorro} aninhos";
+
+
+?>
