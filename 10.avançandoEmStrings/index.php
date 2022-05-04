8bc5883abdcb4c0a0339c671d658d8ae0037d703
Luiz <luis231103@gmail.com>
2022-05-03 23:39:29 -0300

    Funções print e printf

--- 10.avançandoEmStrings/index.php
+++ 10.avançandoEmStrings/index.php
@@ -8,9 +8,41 @@ $idadeCachorro = 10;
 //Sempre lembrar de seguir o padrão do projeto se começar de um jeito termine desse jeito e nunca misture.
 
 echo "Meu cachorro se chama $nomeCachorro e tem $idadeCachorro aninhos";
+echo "<br>";
 echo "Meu cachorro se chama {$nomeCachorro} e tem {$idadeCachorro} aninhos";
+
+echo "<br>";
+echo "<br>";
 
 //Valores de Escape somente são utilizados em texto puro do php
 //simbologia /n -> pular linha. // -> barra invertida. /t -> tab. /$ -> sinal de dolar
+
+//Função print
+
+$msg = "Meu nome é julia";
+
+print("Hello Mundo!!!! $msg");
+
+echo "<br>";
+echo "<br>";
+
+//printf
+
+$name = "Louis";
+
+// %s para strings
+
+printf("Meu nome é %s <br>"  , $name);
+
+// %d para inteiros
+
+$idade = 18;
+
+printf("Tenho %d anos <br>", $idade);
+
+// %float
+
+printf("%.2f m", 1.85);
+
 
 ?>
