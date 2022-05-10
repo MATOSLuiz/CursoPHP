d0872de490b0233938f5675c744944bb1abc69da
Luiz <luis231103@gmail.com>
2022-05-09 21:58:59 -0300

    Adicionando valores Arrays

---
+++ 11-avançandoEmArrays/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+//Adicionando valores em arrays.
+
+$arr = [];
+print_r($arr);
+echo "<br>";
+
+$arr[0] = 15;
+
+echo "<br>";
+print_r($arr);
+
+$arrAssoc = [];
+print_r($arrAssoc);
+echo "<br>";
+
+//Adicionando valores em arrays Associativos.
+
+$arrAssoc["piscina"] = "igui";
+echo "<br>";
+print_r($arrAssoc);
+
+?>
