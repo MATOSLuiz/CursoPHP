12ecf7a89fd9abbade7843692d2fa4e007eaacb6
Luiz <luis231103@gmail.com>
2022-05-09 22:03:53 -0300

    Adicionando valores ao fim do array

--- 11-avançandoEmArrays/index.php
+++ 11-avançandoEmArrays/index.php
@@ -1,24 +1,46 @@
 <?php 
 
-//Adicionando valores em arrays.
+// //Adicionando valores em arrays.
 
-$arr = [];
+// $arr = [];
+// print_r($arr);
+// echo "<br>";
+
+// $arr[0] = 15;
+
+// echo "<br>";
+// print_r($arr);
+
+// $arrAssoc = [];
+// print_r($arrAssoc);
+// echo "<br>";
+
+// //Adicionando valores em arrays Associativos.
+
+// $arrAssoc["piscina"] = "igui";
+// echo "<br>";
+// print_r($arrAssoc);
+
+
+//Adicionando valores ao fim do array
+
+$arr = [1,2,3];
+
+$arr[] = 4;
+
+echo "<br>";
 print_r($arr);
+
+$arr[] = 5;
+
 echo "<br>";
+print_r($arr);  
 
-$arr[0] = 15;
+$arr2 = [];
+
+$arr2[] = 2;
 
 echo "<br>";
-print_r($arr);
-
-$arrAssoc = [];
-print_r($arrAssoc);
-echo "<br>";
-
-//Adicionando valores em arrays Associativos.
-
-$arrAssoc["piscina"] = "igui";
-echo "<br>";
-print_r($arrAssoc);
+print_r($arr2);
 
 ?>
