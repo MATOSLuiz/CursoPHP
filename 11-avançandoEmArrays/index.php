abde248c5d82c081c5d911a85b0494a2ca33b3ec
Luiz <luis231103@gmail.com>
2022-05-09 22:08:04 -0300

    Criando array com a função range

--- 11-avançandoEmArrays/index.php
+++ 11-avançandoEmArrays/index.php
@@ -24,23 +24,34 @@
 
 //Adicionando valores ao fim do array
 
-$arr = [1,2,3];
+// $arr = [1,2,3];
 
-$arr[] = 4;
+// $arr[] = 4;
 
-echo "<br>";
+// echo "<br>";
+// print_r($arr);
+
+// $arr[] = 5;
+
+// echo "<br>";
+// print_r($arr);  
+
+// $arr2 = [];
+
+// $arr2[] = 2;
+
+// echo "<br>";
+// print_r($arr2);
+
+
+//Função range para criar um array rapidamente
+
+$arr = range(0,10);
+
 print_r($arr);
-
-$arr[] = 5;
-
 echo "<br>";
-print_r($arr);  
 
-$arr2 = [];
-
-$arr2[] = 2;
-
-echo "<br>";
+//com step aqui no caso de 0 a 1000 de 100 em 100
+$arr2 = range(0,1000,100);
 print_r($arr2);
-
 ?>
