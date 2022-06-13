d0f8e384772e619dd61be60c4f1e91313d68830f
Luiz <luis231103@gmail.com>
2022-06-13 15:30:04 -0300

    Exercicio 47

---
+++ Exercicios/Exercicio47.php
@@ -0,0 +1,19 @@
+<?php
+
+$carro = [
+    "Jaguar",
+    "3.0",
+    "Azul",
+    18,
+    "Teto Solar",
+    "Automático"
+];
+
+list($modelo,$motor,$cor,$aro,$opcional,$cambio) = $carro;
+
+echo "$modelo<br>";
+echo "$motor<br>";
+echo "$cor<br>";
+echo "$aro<br>";
+echo "$opcional<br>";
+echo "$cambio<br>";
